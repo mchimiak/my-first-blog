7e1bb1b960ff16e1c53726ba318b29810d111285
mchimiak <marta.chimiak@stxnext.pl>
2017-06-07 17:30:27 +0200

    add host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%tj*9_0)e-jaxaym6pke=kn00zqn!x&i059@&yqv-g(=0$-i%_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://marcik.pythonanywhere.com/']
 
 
 # Application definition
